b9982f61280e23574d7623f1c7f8591ee24f5d8f
Earl John Minoza <nap.earljohnminoza@gmail.com>
2022-04-27 16:35:30 +0800

    finished lesson 5

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link :to="{ name: 'EventList' }">Events</router-link> |
-      <router-link :to="{ name: 'About' }">About</router-link>
-      <router-link to="/">Earl Gwapo</router-link> |
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
     <router-view />
   </div>
@@ -29,9 +28,5 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
-}
-
-h4 {
-  font-size: 20px;
 }
 </style>
